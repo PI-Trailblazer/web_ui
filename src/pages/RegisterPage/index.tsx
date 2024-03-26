32c263c433ffb501b1cb64d39731ef4078380a28
MrLoydHD <hf.correia@ua.pt>
2024-03-26 03:38:58 +0000

    TRAIL-34: Feat: Register Page Beautified
    Next step: tranfer register forms into a separated component

--- src/pages/RegisterPage/index.tsx
+++ src/pages/RegisterPage/index.tsx
@@ -25,6 +25,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Checkbox } from "@/components/ui/checkbox"
 import { Loader2 } from "lucide-react"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 const regiterSchema = z.object({
     email: z.string().email(),
@@ -130,87 +131,179 @@ export default function RegisterPage() {
     }
 
     return (
-        <div className='flex flex-col items-center'>
-            <h1>Register Page</h1>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 w-1/6">
-                    <FormField
-                        control={form.control}
-                        name="email"
-                        render={({ field }) => (
-                            <FormItem className='mb-6'>
-                                <FormLabel>Email</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Email" {...field} />
-                                </FormControl>
-                            <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="phone"
-                        render={({ field }) => (
-                            <FormItem className='mb-6'>
-                                <FormLabel>Phone Number</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="1234567890" {...field} />
-                                </FormControl>
-                            <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="password"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Password</FormLabel>
-                                <FormControl>
-                                    <div className='flex flex-col space-y-4'>
-                                        <Input type={showPassword ? "text" : "password"} placeholder="Password" {...field} />
-                                        <div className='items-top flex space-x-2'>
-                                            <Checkbox id='showPassword' onClick={() => setShowPassword(!showPassword)}> Show Password</Checkbox>
-                                            <label
-                                              htmlFor="showPassword"
-                                              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                                            >
-                                              Show Password
-                                            </label>
-                                        </div>
-                                    </div>
-                                </FormControl>
+        <div className='flex flex-col min-h-screen items-center justify-center'>
+            <div className='pb-6'>
+                <h1 className='font-bold text-2xl'>Register Page</h1>
+            </div>
+            <Tabs className='border rounded-xl shadow-lg dark:bg-zinc-900 bg-muted p-8' defaultValue='Tourist'>
+                <TabsList className="grid grid-cols-2 w-[350px]">
+                    <TabsTrigger value='Tourist'>Tourist</TabsTrigger>
+                    <TabsTrigger value='Provider'>Provider</TabsTrigger>
+                </TabsList>
+                <TabsContent value='Tourist'>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                            <FormField
+                                control={form.control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem className='mb-6'>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Email" {...field} />
+                                        </FormControl>
+                                    <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="phone"
+                                render={({ field }) => (
+                                    <FormItem className='mb-6'>
+                                        <FormLabel>Phone Number</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="1234567890" {...field} />
+                                    </FormControl>
                                 <FormMessage />
                             </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="verifyPassword"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Verify Password</FormLabel>
-                                <FormControl>
-                                    <div className='flex flex-col space-y-4'>
-                                        <Input type={showVerifyPassword ? "text" : "password"} placeholder="Verify Password" {...field} />
-                                        <div className='items-top flex space-x-2'>
-                                            <Checkbox id='showVerifyPassword' onClick={() => setShowVerifyPassword(!showVerifyPassword)}> Show Password</Checkbox>
-                                            <label
-                                              htmlFor="showVerifyPassword"
-                                              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                                            >
-                                              Show Password
-                                            </label>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <div className='flex flex-col space-y-4'>
+                                            <Input type={showPassword ? "text" : "password"} placeholder="Password" {...field} />
+                                            <div className='items-top flex space-x-2'>
+                                                <Checkbox id='showPassword' onClick={() => setShowPassword(!showPassword)}> Show Password</Checkbox>
+                                                <label
+                                                htmlFor="showPassword"
+                                                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                                >
+                                                Show Password
+                                                </label>
+                                            </div>
                                         </div>
-                                    </div>
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <Button disabled={isLoading} type="submit">{isLoading && <Loader2 className='mr-2 h-4 w-4 animate-spin' />} Submit</Button>
-                </form>
-            </Form>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="verifyPassword"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Verify Password</FormLabel>
+                                    <FormControl>
+                                        <div className='flex flex-col space-y-4'>
+                                            <Input type={showVerifyPassword ? "text" : "password"} placeholder="Verify Password" {...field} />
+                                            <div className='items-top flex space-x-2'>
+                                                <Checkbox id='showVerifyPassword' onClick={() => setShowVerifyPassword(!showVerifyPassword)}> Show Password</Checkbox>
+                                                <label
+                                                htmlFor="showVerifyPassword"
+                                                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                                >
+                                                Show Password
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button disabled={isLoading} type="submit">{isLoading && <Loader2 className='mr-2 h-4 w-4 animate-spin' />} Submit</Button>
+                    </form>
+                </Form>
+
+                </TabsContent>
+                <TabsContent value='Provider'>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                            <FormField
+                                control={form.control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem className='mb-6'>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Email" {...field} />
+                                        </FormControl>
+                                    <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="phone"
+                                render={({ field }) => (
+                                    <FormItem className='mb-6'>
+                                        <FormLabel>Phone Number</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="1234567890" {...field} />
+                                        </FormControl>
+                                    <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="password"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Password</FormLabel>
+                                        <FormControl>
+                                            <div className='flex flex-col space-y-4'>
+                                                <Input type={showPassword ? "text" : "password"} placeholder="Password" {...field} />
+                                                <div className='items-top flex space-x-2'>
+                                                    <Checkbox id='showPassword' onClick={() => setShowPassword(!showPassword)}> Show Password</Checkbox>
+                                                    <label
+                                                    htmlFor="showPassword"
+                                                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                                    >
+                                                    Show Password
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="verifyPassword"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Verify Password</FormLabel>
+                                        <FormControl>
+                                            <div className='flex flex-col space-y-4'>
+                                                <Input type={showVerifyPassword ? "text" : "password"} placeholder="Verify Password" {...field} />
+                                                <div className='items-top flex space-x-2'>
+                                                    <Checkbox id='showVerifyPassword' onClick={() => setShowVerifyPassword(!showVerifyPassword)}> Show Password</Checkbox>
+                                                    <label
+                                                    htmlFor="showVerifyPassword"
+                                                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                                    >
+                                                    Show Password
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <Button disabled={isLoading} type="submit">{isLoading && <Loader2 className='mr-2 h-4 w-4 animate-spin' />} Submit</Button>
+                        </form>
+                    </Form>
+                </TabsContent>
+            </Tabs>
         </div>
     )    
 }
