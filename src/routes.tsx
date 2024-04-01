75e644ed808476c367b109569ed80ae61f13f34e
LeikRad <48689668+LeikRad@users.noreply.github.com>
2024-04-01 06:45:10 +0100

    Added protected routes, removed React Auth Kit

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,60 +1,81 @@
-import { CleanLayout} from "./layouts/Layout";
-import { lazy, Suspense } from 'react';
+import { CleanLayout } from './layouts/Layout';
+import { lazy, Suspense, ReactNode } from 'react';
+import { useUserStore } from './stores/useUserStore';
+import { Navigate } from 'react-router-dom';
 
-const LandingPage = lazy(() => import("./pages/LandingPage"));
-const ComponentsPage = lazy(() => import("./pages/ComponentsPage"));
-const RegisterPage = lazy(() => import("./pages/RegisterPage"));
-const LoginPage = lazy(() => import("./pages/LoginPage"));
-const OfferListPage = lazy(() => import("./pages/OfferListPage"));
+const LandingPage = lazy(() => import('./pages/LandingPage'));
+const ComponentsPage = lazy(() => import('./pages/ComponentsPage'));
+const RegisterPage = lazy(() => import('./pages/RegisterPage'));
+const LoginPage = lazy(() => import('./pages/LoginPage'));
+const OfferListPage = lazy(() => import('./pages/OfferListPage'));
+
+function ProtectedRoute({
+    children,
+    loggedIn = true,
+    redirect = '/login',
+}: {
+    children: ReactNode;
+    loggedIn?: boolean;
+    redirect?: string;
+}) {
+    const { token } = useUserStore(state => state);
+    console.log('token', token);
+    if (!!token === loggedIn) return children;
+    else return <Navigate to={redirect} />;
+}
 
 const routes = [
     {
-        path: "/",
+        path: '/',
         element: <CleanLayout />,
         children: [
             {
-                path: "/",
-                element: 
-                <Suspense fallback={<div>Loading...</div>}>
-                    <LandingPage />
-                </Suspense>,
-                exact: true
+                path: '/',
+                element: (
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <LandingPage />
+                    </Suspense>
+                ),
+                exact: true,
             },
             {
-                path: "/component",
-                element:
-                <Suspense fallback={<div>Loading...</div>}>
-                    <ComponentsPage />
-                </Suspense>,
-                exact: true
+                path: '/component',
+                element: (
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <ComponentsPage />
+                    </Suspense>
+                ),
+                exact: true,
             },
             {
-                path: "/register",
-                element:
-                <Suspense fallback={<div>Loading...</div>}>
-                    <RegisterPage />
-                </Suspense>,
-                exact: true
+                path: '/register',
+                element: (
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <RegisterPage />
+                    </Suspense>
+                ),
+                exact: true,
             },
             {
-                path: "/login",
-                element:
-                <Suspense fallback={<div>Loading...</div>}>
-                    <LoginPage />
-                </Suspense>,
-                exact: true
+                path: '/login',
+                element: (
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <LoginPage />
+                    </Suspense>
+                ),
+                exact: true,
             },
             {
-                path: "/offer-list",
-                element:
-                <Suspense fallback={<div>Loading...</div>}>
-                    <OfferListPage />
-                </Suspense>,
-                exact: true
-            }
-
-        ]
-    }
+                path: '/offer-list',
+                element: (
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <OfferListPage />
+                    </Suspense>
+                ),
+                exact: true,
+            },
+        ],
+    },
 ];
 
 export { routes };
